036cc320f9f79ebb317bb90ad7c3d4e4518e0502
Smexey <44965371+Smexey@users.noreply.github.com>
2026-05-26 17:13:20 +0200

    wip: assets

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -7,29 +7,30 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {};
+type ExtensionPreferences = {}
 
 /** Preferences accessible in all the extension's commands */
-declare type Preferences = ExtensionPreferences;
+declare type Preferences = ExtensionPreferences
 
 declare namespace Preferences {
   /** Preferences accessible in the `index` command */
-  export type Index = ExtensionPreferences & {};
+  export type Index = ExtensionPreferences & {}
   /** Preferences accessible in the `increase-cortisol` command */
-  export type IncreaseCortisol = ExtensionPreferences & {};
+  export type IncreaseCortisol = ExtensionPreferences & {}
   /** Preferences accessible in the `lower-cortisol` command */
-  export type LowerCortisol = ExtensionPreferences & {};
+  export type LowerCortisol = ExtensionPreferences & {}
   /** Preferences accessible in the `cortisol-menu-bar` command */
-  export type CortisolMenuBar = ExtensionPreferences & {};
+  export type CortisolMenuBar = ExtensionPreferences & {}
 }
 
 declare namespace Arguments {
   /** Arguments passed to the `index` command */
-  export type Index = {};
+  export type Index = {}
   /** Arguments passed to the `increase-cortisol` command */
-  export type IncreaseCortisol = {};
+  export type IncreaseCortisol = {}
   /** Arguments passed to the `lower-cortisol` command */
-  export type LowerCortisol = {};
+  export type LowerCortisol = {}
   /** Arguments passed to the `cortisol-menu-bar` command */
-  export type CortisolMenuBar = {};
+  export type CortisolMenuBar = {}
 }
+
